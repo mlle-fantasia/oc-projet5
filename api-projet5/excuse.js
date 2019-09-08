a8aca458a8b6ce3a9abeb39460374a472bf27abc
Marina Front <marinafront@hotmail.fr>
2019-09-08 10:32:22 +0200

    refactoriasation génération excuse

--- api-projet5/excuse.js
+++ api-projet5/excuse.js
@@ -4,69 +4,42 @@ function genererLesExcuses(excuses, sexe) {
 		let phraseExcuse = "";
 		for (let i = 0; i < objetExcuse.length; i++) {
 			const motExcuse = objetExcuse[i];
-			if (motExcuse.type === "verbe") {
-				if (objetExcuse[i - 1].mot === "j") {
-					motExcuse.avec === "avoir" ? (phraseExcuse += "'ai ") : (phraseExcuse += "e suis ");
-					if (sexe === "femme") {
-						objetExcuse[i - 1].genre = "2";
-					}
-				} else {
-					if (motExcuse.avec === "avoir") {
-						objetExcuse[i - 1].pluriel === "1" ? (phraseExcuse += "ont ") : (phraseExcuse += "a ");
-					} else {
-						objetExcuse[i - 1].pluriel === "1" ? (phraseExcuse += "sont ") : (phraseExcuse += "est ");
-					}
+			if (typeof motExcuse.mot === "string") {
+				if (motExcuse.mot === "j") {
+					objetExcuse.genre = sexe === "femme" ? "2" : "1";
 				}
-				let index = accorder(objetExcuse[i - 1]);
-				phraseExcuse += motExcuse.mot[index];
-			}
-			if (motExcuse.type === "capacite") {
-				phraseExcuse += "ainsi, ";
-			}
-			if (motExcuse.type !== "verbe" && motExcuse.type !== "calificatiftemps") {
+				if (motExcuse.type === "capacite") {
+					phraseExcuse += "ainsi, ";
+				}
 				phraseExcuse += motExcuse.mot;
-			}
-
-			if (motExcuse.type === "formule") {
-				if (sexe === "femme") {
-					if (motExcuse.feminin === "1") {
-						phraseExcuse += "e";
+				if (motExcuse.type === "temps") {
+					motExcuse.pluriel === "1" ? (phraseExcuse += " ont été pour moi") : (phraseExcuse += " a été pour moi");
+				}
+			} else {
+				if (motExcuse.type === "formule") {
+					phraseExcuse += sexe === "femme" ? motExcuse.mot[1] : motExcuse.mot[0];
+					phraseExcuse += ",";
+				} else {
+					if (motExcuse.type === "verbe") {
+						if (motExcuse.avec === "avoir") {
+							phraseExcuse += objetExcuse[i - 1].mot === "j" ? "'ai " : "";
+							phraseExcuse += objetExcuse[i - 1].pluriel === "1" ? "ont " : "a ";
+						} else {
+							phraseExcuse += objetExcuse[i - 1].mot === "j" ? "e suis " : "";
+							phraseExcuse += objetExcuse[i - 1].pluriel === "1" ? "sont " : "est ";
+						}
+					}
+					let index = accordercomplement(objetExcuse[i - 1]);
+					phraseExcuse += motExcuse.mot[index];
+					if (motExcuse.type === "calificatiftemps") {
+						phraseExcuse += " en effet,";
 					}
 				}
-				phraseExcuse += ",";
 			}
-			if (motExcuse.type === "temps") {
-				motExcuse.pluriel === "1" ? (phraseExcuse += " ont été pour moi") : (phraseExcuse += " a été pour moi");
-			}
-			if (motExcuse.type === "calificatiftemps") {
-				let index = accorder(objetExcuse[i - 1]);
-				phraseExcuse += motExcuse.mot[index];
-				phraseExcuse += " en effet,";
-			}
-			motExcuse.mot === "j" ? (phraseExcuse += "") : (phraseExcuse += " ");
+			phraseExcuse += motExcuse.mot === "j" ? "" : " ";
 		}
 		tabDExcuses.push(phraseExcuse);
 	});
 	console.log("tabDExcuses", tabDExcuses);
 	return tabDExcuses;
-}
-
-// fonction qui renvoie le bon index pour accorder à l'objet passé en paramettre
-function accorder(objetsujet) {
-	let index = 0;
-	if (objetsujet.genre === "1") {
-		if (objetsujet.pluriel === "1") {
-			index = 2;
-		} else {
-			index = 0;
-		}
-	}
-	if (objetsujet.genre === "2") {
-		if (objetsujet.pluriel === "1") {
-			index = 3;
-		} else {
-			index = 1;
-		}
-	}
-	return index;
 }
